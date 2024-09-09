e9c20ff478253529ed828b626b8b2284f6906305
Pablo Saiz <pablo.saiz@cern.ch>
2024-09-09 15:32:15 +0200

    WIP: Updating the developer's guide

---
+++ scripts/start-server-debug.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Ready to start the web server in debug mode"
+cd /code
+export INVENIO_SECRET_KEY=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | fold -w 10 | head -n 1)
+if [ -d "/opt/invenio/var/instance/python/lib/python3.9/site-packages/cernopendata" ]; then
+  echo "The installation directory is still there... let's overwrite it"
+  cp /code/cernopendata/setup.py /code/setup.py
+  pip install -e .
+
+  if [ -d "/cold" ]; then
+    cd /cold
+    pip install -e .
+  fi
+fi
+
+if [ "$1"  == "worker" ]; then
+  echo "Starting the celery worker"
+  celery -A cernopendata.celery worker --beat --loglevel=INFO --concurrency=1
+else
+  echo "Starting the web server"
+  cernopendata run -h 0.0.0.0 --reload;
+fi
+echo "THE WEB SERVICE DIED!!! Let's sleep for a bit to give some time to debug"
+sleep 60
