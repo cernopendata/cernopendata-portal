7cbd67ba10504541d8f2278c7ca934132ec833c1
David Horvát <horvatda@proton.me>
2024-12-09 10:18:37 +0100

    Config: Added tests and applied lining fixes

--- cernopendata/modules/globals/ext.py
+++ cernopendata/modules/globals/ext.py
@@ -60,7 +60,7 @@ class GlobalVariables:
         # check config for custom setting
         if exclude_experiments := app.config.get("EXCLUDE_EXPERIMENTS"):
             if isinstance(exclude_experiments, str):
-                exclude_experiments = json.loads(exclude_experiments.replace("'", "\""))
+                exclude_experiments = json.loads(exclude_experiments.replace("'", '"'))
 
                 logger.info("Loaded experiments from string")
 
