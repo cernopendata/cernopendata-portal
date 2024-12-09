0ee53e30718d535d70527638d8568f51ee28777a
David Horvát <horvatda@proton.me>
2024-12-09 10:52:13 +0100

    Anniversary: Removed commented-out code

--- cernopendata/modules/theme/assets/semantic-ui/js/anniversary.js
+++ cernopendata/modules/theme/assets/semantic-ui/js/anniversary.js
@@ -8,7 +8,6 @@ const unlock_date = new Date("2024-12-10T16:00Z")
 
 function Celebrate() {
     const [stateShowConfetti, setShowConfetti] = useState(false);
-    // const [stateShowBlog, setShowBlog] = useState(false);
 
     let make_confetti = () => {
         if (stateShowConfetti) return;
