533171997ee738c837dfbd011c807bc6b5245e14
Pablo Saiz <pablo.saiz@cern.ch>
2026-05-22 09:06:06 +0200

    feat(version): proof of concept of adding version control to the records

--- cernopendata/modules/records/minters/recid.py
+++ cernopendata/modules/records/minters/recid.py
@@ -1,30 +1,11 @@
 """PID minters."""
 
-from invenio_oaiserver.provider import OAIIDProvider
-
 from ..providers.recid import RecordUUIDProvider
+from .opendata import cernopendata_generic_minter
 
 
 def cernopendata_recid_minter(record_uuid, data):
     """Mint deposit's PID."""
-    if "id" in data:
-        recid = data["id"]
-    else:
-        recid = data["recid"]
-
-    provider = RecordUUIDProvider.create(
-        object_type="rec",
-        pid_type="recid",
-        object_uuid=record_uuid,
-        pid_value=str(recid),
+    return cernopendata_generic_minter(
+        record_uuid, data, "recid", "recid", RecordUUIDProvider, oai=True
     )
-
-    data["pids"] = {"oai": {"id": f"oai:cernopendata.cern:{recid}"}}
-
-    OAIIDProvider.create(
-        object_type="rec",
-        object_uuid=record_uuid,
-        pid_value=f"oai:cernopendata.cern:{recid}",
-    )
-
-    return provider.pid
